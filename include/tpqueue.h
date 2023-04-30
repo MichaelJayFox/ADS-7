49026d201e62e2c1bced2c66e22ede0603fe873c
MichaelJayFox <125122867+MichaelJayFox@users.noreply.github.com>
2023-04-30 23:15:49 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,10 +2,84 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <string>
+
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct Queue{
+      T value;
+      Queue* next;
+      Queue* prev;
+    };
+    Queue* head;
+    Queue* create(const T& value) {
+        Queue* temp = new Queue;
+        Queue->value = value;
+        Queue->next = nullptr;
+        return temp;
+    } 
+  
+  public:
+    TPQueue() : head(nullptr) {}
+    void push(const T& item);
+    T pop();
+    ~TPQueue() {
+        while (head) {
+        Item* tmp = head->next;
+        delete head;
+        head = tmp;
+       }
+    }
+    TPQueue() {
+      head = nullptr;
+    }
+    bool Empty() {
+      return head == nullptr;
+    }
+    T pop() {
+      if (Empty()) {
+        throw std::string("Empty!");
+      } else {
+        Queue* Queue = head->next;
+        T i = head->val;
+        delete head;
+        head = Queue;
+        return i;
+      }
+    }  
 };
+template<typename T>
+void TPQueue<T>::push(const T& i) {
+    if (head == nullptr) {
+        head = create(i);
+    } else {
+        Item* cur = head;
+        int l = 0;
+        while (cur) {
+            if (cur->value.prior < i.prior) {
+                break;
+            }
+            cur = cur->next;
+            l++;
+        }
+        if (l == 0) {
+            Item* tmp = new Item;
+            tmp->next = head;
+            tmp->value = i;
+            head = tmp;
+        } else {
+            cur = head;
+            for (int k = 1; k < l; k++) {
+                cur = cur->next;
+            }
+            Item* tmp = new Item;
+            tmp->next = cur->next;
+            tmp->value = i;
+            cur->next = tmp;
+        }
+    }
+}
 
 struct SYM {
   char ch;
